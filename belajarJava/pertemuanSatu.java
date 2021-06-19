9bcc22ca23858fd3be34ebc2678a0ce6bddce9c8
Rahmat <dn.rahmath@gmail.com>
2021-06-19 08:07:07 +0700

    a

--- belajarJava/pertemuanSatu.java
+++ belajarJava/pertemuanSatu.java
@@ -1,42 +1,34 @@
 package belajarJava;
 
-public class pertemuanSatu
-{
-    public static void main(String args[]){  
+public class pertemuanSatu {
+    public static void main(String args[]) {
 
-      System.out.println("Hello world");  
-      
-      //comments
-      /* enter
-       * enter
-       * comments
-       */
-      
-      /**/
+        System.out.println("Hello world");
 
-      //----------------------------------------------------
+        // comments
+        /*
+         * enter enter comments
+         */
 
-      //variabel
-      String a = "ini String";    // ini type data String
-      int b = 10;    // ini type data Integer
+        /**/
 
-      System.out.println(a);  
-      System.out.println(b);  
+        // ----------------------------------------------------
 
+        // variabel
+        String a = "ini String"; // ini type data String
+        int b = 10; // ini type data Integer
 
+        System.out.println(a);
+        System.out.println(b);
 
-      // --- Operator Relasional (==,!=,>=,<=,>,<) Operator Pembanding
-      // --- Operator Logic (&&,||) Operator Logika
+        // --- Operator Relasional (==,!=,>=,<=,>,<) Operator Pembanding
+        // --- Operator Logic (&&,||) Operator Logika
 
-      //disjungsi (atau)
-      boolean c = 10 == 10 || 10 == 10; 
-      
-      //konjungsi (dan)
-      boolean d = 10 == 10 && 10 == 10; 
+        
 
-      System.out.println(c);  
-      System.out.println(d);
+        System.out.println(c);
+        System.out.println(d);
 
-      System.out.println(!c);  //operator negasi (bukan)
-  }  
+        System.out.println(!c); // operator negasi (bukan)
+    }
 }
